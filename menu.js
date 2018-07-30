d3a86de968d65fe4aee7c212e8076cfe65138f1a
EC2 Default User <ec2-user@ip-172-31-47-140.us-east-2.compute.internal>
2018-07-30 08:07:38 +0000

    c

--- menu.js
+++ menu.js
@@ -30,6 +30,8 @@ $(function(){
 });
 
 
+
+
 //スライダー
 
 $(function(){
@@ -134,8 +136,26 @@ return false;
 
 $(function(){
  var maxvh = $(window).outerHeight();
- $('.top-cover__img').css('height',maxvh*0.4+'px');
- $('h1').css('height',maxvh*0.52+'px');
- $('.container').css('height',maxvh*0.96+'px');
+// .header-icon__ul height 6vh
+// .header-icon__ul margin-top 2vh
+// h1 padding-top 8vh
+
+ $('.header-icon__ul').css({height:maxvh*0.06+'px', 'margin-top':maxvh*0.02+'px'});
+ $('h1').css({height:maxvh*0.52+'px','padding-top':maxvh*0.08+'px'});
+ $('.container').css({height:maxvh*0.96+'px',top:maxvh*0.02+'px',bottom:maxvh*0.02+'px'});
+ $('.profile-content').css({height:maxvh*0.9+'px','margin-top':maxvh*0.06+'px'});
+ $('.sp-profile-img__img').css({height:maxvh*0.25+'px','margin-top':maxvh*0.03+'px'});
+ $('.profile-txt p').css({'font-size':maxvh*0.022+'px'});
+ $('.profile-txt p span').css({'font-size':maxvh*0.016+'px'});
+ $('.albumlog-content').css({'padding-top':maxvh*0.06+'px','padding-bottom':maxvh*0.03+'px'});
+ $('.albumlog-txt').css({'margin-top':maxvh*0.01+'px','margin-bottom':maxvh*0.01+'px',height:maxvh*0.50+'px'});
+ $('.albumlog-txt p, .albumlog-txt h2').css({'font-size':maxvh*0.016+'px'});
+ $('.cover-box').css({height:maxvh*0.28+'px'});
+ $('.top-cover__img').css({height:maxvh*0.4+'px'});
+ $('.slider-pro p').css({height:maxvh*0.5+'px'});
+ $('.heightFix').css({height:maxvh*0.95+'px'});
+ 
+ 
+
 
 });
